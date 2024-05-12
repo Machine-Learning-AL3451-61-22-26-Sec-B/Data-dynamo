f2fe9f8eb56cd07b3a64fcb1e5267d14c53cc044
Thrisharadhakrishnan <164322755+Thrisharadhakrishnan@users.noreply.github.com>
2024-05-12 20:39:53 +0530

    Update naive.py

--- naive.py
+++ naive.py
@@ -1,44 +1,35 @@
+import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# load data from CSV
-data = pd.read_csv('tennisdata.csv')
-print("THe first 5 values of data is :\n",data.head())
+# Load data from CSV
+@st.cache
+def load_data():
+    return pd.read_csv('tennisdata.csv')
 
-# obtain Train data and Train output
-X = data.iloc[:,:-1]
-print("\nThe First 5 values of train data is\n",X.head())
+data = load_data()
 
-y = data.iloc[:,-1]
-print("\nThe first 5 values of Train output is\n",y.head())
+# Preprocessing
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
-# Convert then in numbers 
-le_outlook = LabelEncoder()
-X.Outlook = le_outlook.fit_transform(X.Outlook)
+le = LabelEncoder()
+X = X.apply(le.fit_transform)
+y = le.fit_transform(y)
 
-le_Temperature = LabelEncoder()
-X.Temperature = le_Temperature.fit_transform(X.Temperature)
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-le_Humidity = LabelEncoder()
-X.Humidity = le_Humidity.fit_transform(X.Humidity)
-
-le_Windy = LabelEncoder()
-X.Windy = le_Windy.fit_transform(X.Windy)
-
-print("\nNow the Train data is :\n",X.head())
-
-le_PlayTennis = LabelEncoder()
-y = le_PlayTennis.fit_transform(y)
-print("\nNow the Train output is\n",y)
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.20)
-
+# Training the model
 classifier = GaussianNB()
-classifier.fit(X_train,y_train)
+classifier.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
-print("Accuracy is:",accuracy_score(classifier.predict(X_test),y_test))
+# Evaluating the model
+y_pred = classifier.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+# Displaying accuracy
+st.write("Accuracy:", accuracy)
